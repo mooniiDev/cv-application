0f6bcb837aff38918a275fd2d6cb7c3ff73d0c72
mooniiDev <moonii@riseup.net>
2022-05-26 14:06:48 +0300

    Add Main component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import './styles/App.css';
 import Header from './components/Header';
+import Main from './components/Main';
 
 class App extends Component {
   constructor() {
@@ -13,6 +14,7 @@ class App extends Component {
     return (
       <div>
         <Header />
+        <Main />
       </div>
     );
   }
---
+++ src/components/Main.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <main id="main">
+        <p>Main Content</p>
+      </main>
+    );
+  }
+}
+
+export default Main;
