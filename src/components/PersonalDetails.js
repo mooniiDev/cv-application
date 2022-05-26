edd0095bad9abd77892f228cb7042f37c17f02ce
mooniiDev <moonii@riseup.net>
2022-05-26 19:09:28 +0300

    Update PersonalDetails component layout and classes

--- src/components/PersonalDetails.js
+++ src/components/PersonalDetails.js
@@ -9,37 +9,37 @@ class PersonalDetails extends React.Component {
 
   render() {
     return (
-      <div className="PersonalDetails">
-        <h2>PERSONAL DETAILS</h2>
-        <form>
-          <div className="person-div">
-            <div className="name">
-              <label htmlFor="name">Name</label>
-              <input id="name" type="text" required />
+      <div>
+        <h2 className="personal-heading">PERSONAL DETAILS</h2>
+        <form className="personal-details">
+          <div className="personal-info-div">
+            <div>
+              <label htmlFor="personal-name">Name</label>
+              <input id="personal-name" type="text" required />
             </div>
-            <div className="surname">
-              <label htmlFor="surname">Surname</label>
-              <input id="surname" type="text" required />
-            </div>
-          </div>
-          <div className="contacts-div">
-            <div className="phone">
-              <label htmlFor="phone">Phone</label>
-              <input id="phone" type="text" />
-            </div>
-            <div className="email">
-              <label htmlFor="email">Email</label>
-              <input id="email" type="text" required />
+            <div>
+              <label htmlFor="personal-surname">Surname</label>
+              <input id="personal-surname" type="text" required />
             </div>
           </div>
-          <div className="socials-div">
-            <div className="linkedin">
-              <label htmlFor="linkedin">LinkedIn</label>
-              <input id="linkedin" type="text" />
+          <div className="personal-contacts-div">
+            <div>
+              <label htmlFor="personal-phone">Phone</label>
+              <input id="personal-phone" type="text" />
             </div>
-            <div className="github">
-              <label htmlFor="github">GitHub</label>
-              <input id="github" type="text" />
+            <div>
+              <label htmlFor="personal-email">Email</label>
+              <input id="personal-email" type="text" required />
+            </div>
+          </div>
+          <div className="personal-socials-div">
+            <div>
+              <label htmlFor="personal-linkedin">LinkedIn</label>
+              <input id="personal-linkedin" type="text" />
+            </div>
+            <div>
+              <label htmlFor="personal-github">GitHub</label>
+              <input id="personal-github" type="text" />
             </div>
           </div>
           <input type="submit" value="Save" />
