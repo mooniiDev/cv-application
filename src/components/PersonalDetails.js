27d0c6710929fa236679ffc06a127750ea023548
mooniiDev <moonii@riseup.net>
2022-05-27 13:57:32 +0300

    Update attributes of PersonalDetails inputs

--- src/components/PersonalDetails.js
+++ src/components/PersonalDetails.js
@@ -15,31 +15,50 @@ class PersonalDetails extends React.Component {
           <div className="personal-info-div">
             <div>
               <label htmlFor="personal-name">Name</label>
-              <input id="personal-name" type="text" required />
+              <input
+                id="personal-name"
+                type="text"
+                name="personal-name"
+                required
+              />
             </div>
             <div>
               <label htmlFor="personal-surname">Surname</label>
-              <input id="personal-surname" type="text" required />
+              <input
+                id="personal-surname"
+                type="text"
+                name="surname"
+                required
+              />
             </div>
           </div>
           <div className="personal-contacts-div">
             <div>
               <label htmlFor="personal-phone">Phone</label>
-              <input id="personal-phone" type="text" />
+              <input
+                id="personal-phone"
+                type="tel"
+                name="personal-phone"
+                required
+              />
             </div>
             <div>
               <label htmlFor="personal-email">Email</label>
-              <input id="personal-email" type="text" required />
+              <input id="personal-email" type="email" name="email" required />
             </div>
           </div>
           <div className="personal-socials-div">
             <div>
               <label htmlFor="personal-linkedin">LinkedIn</label>
-              <input id="personal-linkedin" type="text" />
+              <input
+                id="personal-linkedin"
+                type="text"
+                name="personal-linkedin"
+              />
             </div>
             <div>
               <label htmlFor="personal-github">GitHub</label>
-              <input id="personal-github" type="text" />
+              <input id="personal-github" type="text" name="personal-github" />
             </div>
           </div>
           <input type="submit" value="Save" />
