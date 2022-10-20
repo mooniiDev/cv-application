ae9255fe7f738280926581e2abef0748fd879942
mooniiDev <mooniidev@proton.me>
2022-10-20 12:27:37 +0300

    Add PreviewCV and EditCV components

---
+++ src/components/EditCV.js
@@ -0,0 +1,16 @@
+// Packages imports
+import React from 'react';
+
+class EditCV extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return <div />;
+  }
+}
+
+export default EditCV;
---
+++ src/components/PreviewCV.js
@@ -0,0 +1,16 @@
+// Packages imports
+import React from 'react';
+
+class PreviewCV extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return <div />;
+  }
+}
+
+export default PreviewCV;
