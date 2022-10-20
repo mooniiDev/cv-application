f759fde7caf1b95f6bd64a0721a855af6e0697e8
mooniiDev <mooniidev@proton.me>
2022-10-20 13:33:03 +0300

    Move main content from Main to EditCV component

--- src/components/EditCV.js
+++ src/components/EditCV.js
@@ -1,5 +1,12 @@
 // Packages imports
 import React from 'react';
+import PropTypes from 'prop-types';
+
+// Components imports
+import PersonalInfo from './PersonalInfo';
+import PracticeInfo from './PracticeInfo';
+import EducationInfo from './EducationInfo';
+import SkillsInfo from './SkillsInfo';
 
 class EditCV extends React.Component {
   constructor(props) {
@@ -9,8 +16,69 @@ class EditCV extends React.Component {
   }
 
   render() {
-    return <div />;
+    const {
+      handleFormDisplay,
+      personalDisplay,
+      practiceDisplay,
+      educationDisplay,
+      skillsDisplay,
+    } = this.props;
+
+    return (
+      <div>
+        <PersonalInfo
+          formType="personal"
+          title="PERSONAL INFORMATION"
+          border="red-border"
+          hover="red-hover"
+          formVisibility={personalDisplay}
+          handleFormDisplay={handleFormDisplay}
+        />
+        <PracticeInfo
+          formType="practice"
+          title="PRACTICAL EXPERIENCE"
+          border="blue-border"
+          hover="blue-hover"
+          formVisibility={practiceDisplay}
+          handleFormDisplay={handleFormDisplay}
+        />
+        <EducationInfo
+          formType="education"
+          title="EDUCATIONAL EXPERIENCE"
+          border="orange-border"
+          hover="orange-hover"
+          formVisibility={educationDisplay}
+          handleFormDisplay={handleFormDisplay}
+        />
+        <SkillsInfo
+          formType="skills"
+          title="ADDITIONAL SKILLS"
+          border="purple-border"
+          hover="purple-hover"
+          formVisibility={skillsDisplay}
+          handleFormDisplay={handleFormDisplay}
+        />
+      </div>
+    );
   }
 }
+
+// Validating prop types
+EditCV.propTypes = {
+  personalDisplay: PropTypes.bool,
+  practiceDisplay: PropTypes.bool,
+  educationDisplay: PropTypes.bool,
+  skillsDisplay: PropTypes.bool,
+  handleFormDisplay: PropTypes.func,
+};
+
+// Creating default props
+EditCV.defaultProps = {
+  personalDisplay: false,
+  practiceDisplay: false,
+  educationDisplay: false,
+  skillsDisplay: false,
+  handleFormDisplay: () => {},
+};
 
 export default EditCV;
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,10 +3,8 @@ import React from 'react';
 
 // Components imports
 import Button from './Button';
-import PersonalInfo from './PersonalInfo';
-import PracticeInfo from './PracticeInfo';
-import EducationInfo from './EducationInfo';
-import SkillsInfo from './SkillsInfo';
+import EditCV from './EditCV';
+import PreviewCV from './PreviewCV';
 
 // Styling imports
 import '../styles/Main.css';
@@ -16,10 +14,13 @@ class Main extends React.Component {
     super(props);
 
     this.state = {
-      isPersonalFormDisplayed: false,
-      isPracticeFormDisplayed: false,
-      isEducationFormDisplayed: false,
-      isSkillsFormDisplayed: false,
+      formDisplay: {
+        isPersonalFormDisplayed: false,
+        isPracticeFormDisplayed: false,
+        isEducationFormDisplayed: false,
+        isSkillsFormDisplayed: false,
+      },
+      editMode: true,
     };
   }
 
@@ -31,21 +32,21 @@ class Main extends React.Component {
     // Displaying and hiding personal form
     if (formType === 'personal' && e.target.classList.contains('show')) {
       this.setState({
-        isPersonalFormDisplayed: true,
+        formDisplay: { isPersonalFormDisplayed: true },
       });
     } else if (formType === 'personal' && e.target.classList.contains('hide')) {
       this.setState({
-        isPersonalFormDisplayed: false,
+        formDisplay: { isPersonalFormDisplayed: false },
       });
 
       // Displaying and hiding practice form
     } else if (formType === 'practice' && e.target.classList.contains('show')) {
       this.setState({
-        isPracticeFormDisplayed: true,
+        formDisplay: { isPracticeFormDisplayed: true },
       });
     } else if (formType === 'practice' && e.target.classList.contains('hide')) {
       this.setState({
-        isPracticeFormDisplayed: false,
+        formDisplay: { isPracticeFormDisplayed: false },
       });
 
       // Displaying and hiding education form
@@ -54,72 +55,50 @@ class Main extends React.Component {
       e.target.classList.contains('show')
     ) {
       this.setState({
-        isEducationFormDisplayed: true,
+        formDisplay: { isEducationFormDisplayed: true },
       });
     } else if (
       formType === 'education' &&
       e.target.classList.contains('hide')
     ) {
       this.setState({
-        isEducationFormDisplayed: false,
+        formDisplay: { isEducationFormDisplayed: false },
       });
 
       // Displaying and hiding skills form
     } else if (formType === 'skills' && e.target.classList.contains('show')) {
       this.setState({
-        isSkillsFormDisplayed: true,
+        formDisplay: { isSkillsFormDisplayed: true },
       });
     } else if (formType === 'skills' && e.target.classList.contains('hide')) {
       this.setState({
-        isSkillsFormDisplayed: false,
+        formDisplay: { isSkillsFormDisplayed: false },
       });
     }
   };
 
   render() {
-    const {
-      isPersonalFormDisplayed,
-      isPracticeFormDisplayed,
-      isEducationFormDisplayed,
-      isSkillsFormDisplayed,
-    } = this.state;
+    const { formDisplay, editMode } = this.state;
     const { handleFormDisplay } = this;
 
+    if (editMode === true) {
+      return (
+        <main id="Main">
+          <Button buttonText="PREVIEW" buttonClass="main-button" />
+          <EditCV
+            handleFormDisplay={handleFormDisplay}
+            personalDisplay={formDisplay.isPersonalFormDisplayed}
+            practiceDisplay={formDisplay.isPracticeFormDisplayed}
+            educationDisplay={formDisplay.isEducationFormDisplayed}
+            skillsDisplay={formDisplay.isSkillsFormDisplayed}
+          />
+        </main>
+      );
+    }
     return (
       <main id="Main">
-        <Button buttonText="PREVIEW" buttonClass="preview-button" />
-        <PersonalInfo
-          formType="personal"
-          title="PERSONAL INFORMATION"
-          border="red-border"
-          hover="red-hover"
-          formVisibility={isPersonalFormDisplayed}
-          handleFormDisplay={handleFormDisplay}
-        />
-        <PracticeInfo
-          formType="practice"
-          title="PRACTICAL EXPERIENCE"
-          border="blue-border"
-          hover="blue-hover"
-          formVisibility={isPracticeFormDisplayed}
-          handleFormDisplay={handleFormDisplay}
-        />
-        <EducationInfo
-          formType="education"
-          title="EDUCATIONAL EXPERIENCE"
-          border="orange-border"
-          hover="orange-hover"
-          formVisibility={isEducationFormDisplayed}
-          handleFormDisplay={handleFormDisplay}
-        />
-        <SkillsInfo
-          formType="skills"
-          title="ADDITIONAL SKILLS"
-          border="purple-border"
-          hover="purple-hover"
-          formVisibility={isSkillsFormDisplayed}
-          handleFormDisplay={handleFormDisplay}
-        />
+        <Button buttonText="EDIT" buttonClass="main-button" />
+        <PreviewCV />
       </main>
     );
   }
