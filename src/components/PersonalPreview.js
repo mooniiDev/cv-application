cbf5561a7c5a137ce909b07c979d011974c60e63
mooniiDev <mooniidev@proton.me>
2022-10-25 13:41:29 +0300

    Update PersonalPreview component's layout, give more styling to it

--- src/components/PersonalPreview.js
+++ src/components/PersonalPreview.js
@@ -16,10 +16,37 @@ class PersonalPreview extends React.Component {
     const { cvInfo } = this.props;
     return (
       <div className="personal-preview-box">
+        {/* Person name and surname */}
         <h2 className="person-name">
           {cvInfo.personal.firstName.toUpperCase()}{' '}
           {cvInfo.personal.lastName.toUpperCase()}
         </h2>
+
+        {/* Person contacts */}
+        <div className="person-contacts">
+          {/* Person phone and email */}
+          <div>
+            <p>
+              <span>Phone:</span> &nbsp;{cvInfo.personal.phone}
+            </p>
+            <p>
+              <span>Email:</span> &nbsp;{cvInfo.personal.mail}
+            </p>
+          </div>
+
+          {/* Person socials */}
+          <div>
+            <p>
+              <span>LinkedIn:</span> &nbsp;{cvInfo.personal.linkedin}
+            </p>
+            <p>
+              <span>GitHub:</span> &nbsp;{cvInfo.personal.github}
+            </p>
+          </div>
+        </div>
+
+        {/* Person 'about' information */}
+        <p className="person-about">{cvInfo.personal.about}</p>
       </div>
     );
   }
@@ -31,6 +58,11 @@ PersonalPreview.propTypes = {
     personal: PropTypes.shape({
       firstName: PropTypes.string,
       lastName: PropTypes.string,
+      phone: PropTypes.string,
+      mail: PropTypes.string,
+      linkedin: PropTypes.string,
+      github: PropTypes.string,
+      about: PropTypes.string,
     }),
   }).isRequired,
 };
