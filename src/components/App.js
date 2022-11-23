105c183d8b9b48392413cea9d33d856d4ee1ba45
mooniiDev <mooniidev@proton.me>
2022-11-23 19:12:01 +0200

    Move App.js to components folder

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,9 @@
 import { Component } from 'react';
 
 // Components Imports
-import Header from './components/Header';
-import Main from './components/Main';
-import Footer from './components/Footer';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
 
 class App extends Component {
   constructor() {
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 // React App Import
-import App from './App';
+import App from './components/App';
 
 // Styling Imports
 import './styles/index.css';
